dd24959f6a85501583f3ae20a5721c8fc829b9b0
mahesh <maheshverma.niit@gmail.com>
2023-08-07 18:15:39 +0530

    image

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,17 +6,17 @@ const Navbar = () => {
     <div>
        <nav className="navbar navbar-expand-lg bg-light">
     <div className="container-fluid">
-      <Link className="navbar-brand" to="#">klik-plus</Link>
+      <Link className="navbar-brand " to="#">klik-plus</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="">Home</Link>
+            <Link className="nav-link active" aria-current="page" to="">HOME</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="about">About</Link>
+            <Link className="nav-link" to="about">ABOUT</Link>
           </li>
           
           <li className="nav-item dropdown">
@@ -49,14 +49,9 @@ const Navbar = () => {
             <Link className="nav-link" to="our-clients">OUR CLIENTS</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="contact">Contact</Link>
+            <Link className="nav-link" to="contact">CONTACT</Link>
           </li>
         </ul>
-        
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>

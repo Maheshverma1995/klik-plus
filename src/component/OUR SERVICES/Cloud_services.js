4561180e9cd8154c14b9f3e9a60c42aeb12612a8
mahesh <maheshverma.niit@gmail.com>
2023-08-21 13:05:15 +0530

    cloud

--- src/component/OUR SERVICES/Cloud_services.js
+++ src/component/OUR SERVICES/Cloud_services.js
@@ -1,9 +1,101 @@
 import React from 'react'
+import './Cloud-services.css'
 
 const Cloud_services = () => {
   return (
     <div>
-      cloud services
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col d-flex justify-content-center mt-3 mb-3'>
+            <img className='img-fluid ' src="./Image/Cloud-Services.jpg  " alt="" />
+          </div>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='row text-center'>
+          <h1>
+            What's Included In A Cloud?
+          </h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil sunt quia quos ea qui dignissimos aperiam mollitia fugit porro fugiat. Temporibus dolorum unde nihil quibusdam quis iste assumenda id corporis!</p>
+        </div>
+        <div className='row'>
+          <div className='col-md-4'>
+            <div class="card" >
+              <img src="./Image/Cloud-Services-6.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Lern-more</a>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <div class="card" >
+              <img src="./Image/Cloud-Services-5.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Lern-more</a>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <div class="card" >
+              <img src="./Image/Cloud-Services-4.jpg" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Lern-more</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='container'>
+      <div className='row mt-2 mb-5'>
+        <div className='col-md-6'>
+          <img className='img-fluid' src="./Image/Cloud-Services-3.jpg" alt="" />
+        </div>
+        <div className='col-md-6'>
+          <h1>Lorem ipsum dolor sit</h1>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus sint, sed id explicabo sequi laboriosam quaerat dolorum incidunt aliquam repellendus nesciunt sunt nostrum laborum quisquam consequuntur, iste dolor omnis unde.</p>
+        </div>
+      </div>
+      </div>
+      <div className='container-fluid section-cloud-service'>
+       <div className='container'>
+       <div className='row text-center'>
+        <h6>SERVICES</h6>
+      <h1>Our Featured services</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, excepturi. Minus, esse suscipit? Esse dolor temporibus, unde adipisci, laudantium iste amet illum eius alias doloribus cum dolorem! Est, suscipit blanditiis.</p>
+        </div>
+        <div className='row'>
+          <div className='col-md-6'>
+            <i style={{width:'100px'}}>Icon</i>
+            <h2>Heading</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius voluptatibus velit ea blanditiis totam dolorem, exercitationem animi provident adipisci, veniam doloribus maxime debitis! Culpa ratione dignissimos labore veniam enim quam?</p>
+          </div>
+          <div className='col-md-6'>
+            <i style={{width:'100px'}}>Icon</i>
+            <h2>Heading</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius voluptatibus velit ea blanditiis totam dolorem, exercitationem animi provident adipisci, veniam doloribus maxime debitis! Culpa ratione dignissimos labore veniam enim quam?</p>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-md-6'>
+            <i style={{width:'100px'}}>Icon</i>
+            <h2>Heading</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius voluptatibus velit ea blanditiis totam dolorem, exercitationem animi provident adipisci, veniam doloribus maxime debitis! Culpa ratione dignissimos labore veniam enim quam?</p>
+          </div>
+          <div className='col-md-6'>
+            <i style={{width:'100px'}}>Icon</i>
+            <h2>Heading</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius voluptatibus velit ea blanditiis totam dolorem, exercitationem animi provident adipisci, veniam doloribus maxime debitis! Culpa ratione dignissimos labore veniam enim quam?</p>
+          </div>
+        </div>
+       </div>
+      </div>
     </div>
   )
 }

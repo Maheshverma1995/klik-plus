7e010e8703778c3ed3d6bcc251f3dd40ce691ba7
mahesh <maheshverma.niit@gmail.com>
2023-08-22 17:36:47 +0530

    save

--- src/component/OUR SERVICES/App_Development.js
+++ src/component/OUR SERVICES/App_Development.js
@@ -2,8 +2,75 @@ import React from 'react'
 
 const App_Development = () => {
   return (
-    <div>
-      App_Development
+    <div className='container-fluid'>
+    <div className='row'>
+      <div className='col'>
+        <img className='img-fluid'style={{height:'450px',width:'100%'}} src="./Image/mobile-app-development-1.jpg" alt="" />
+      </div>
+    </div>
+    <div className="container">
+          <div className='row m-5 p-1'>
+            <h1 className='text-center'>WHAT WE DO</h1>
+          </div>
+          <div className='row mt-2 mb-3 p-3'>
+            <div className='col-md-3'>
+              <div class="card" style={{height:'350px'}} >
+                <img src="./Image/digital_marketing.jpg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-3'>
+              <div class="card" style={{height:'350px'}} >
+                <img src="./Image/digital-marketing-2.jpg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-3'>
+              <div class="card" style={{height:'350px'}} >
+                <img src="./Image/digital-marketing-1.jpg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-3'>
+              <div class="card" style={{height:'350px'}} >
+                <img src="./Image/digital-marketing-5.jpg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='row mt-2 mb-3 p-3'>
+                <div className='col-md-6'>
+                  <img className='img-fluid' src="./Image/digital-marketing-.jpg" alt="dfd" />
+                </div>
+                <div className='col-md-6'>
+                <h1>Lorem ipsum dolor sit amet </h1>
+                <h3>my headding</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ratione eaque eveniet optio amet eligendi praesentium natus voluptatum nostrum, culpa atque quas aliquid veniam perspiciatis impedit iste officia. Ea, quos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui, amet placeat! Itaque, porro possimus? Autem repellendus illum sequi sit consequuntur quia asperiores optio ducimus accusamus enim, numquam commodi, quaerat ex.</p>
+                </div>
+          </div>
+          <div className='row'>
+            <div className='col'>
+              <div className='text-center mt-2 mb-3 p-3'>
+                <img className='img-fluid' src="./Image/digital-marketing-4.webp" alt="" />
+                <h1>dfdsdfsd</h1>
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam ipsa, esse eligendi quod dolorem hic perferendis? Perferendis pariatur dolores quis ullam, libero fugiat voluptatibus earum, ipsum, voluptatum assumenda totam tempore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptates dolorum beatae explicabo? Aut placeat inventore saepe laudantium repellat corporis, natus delectus pariatur aliquid tempora, doloribus veritatis sed voluptatem quia.</p>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
--- src/component/OUR SERVICES/Rrp_Development.js
+++ src/component/OUR SERVICES/Rrp_Development.js
@@ -5,13 +5,58 @@ const Rrp_Development = () => {
     <div className='container-fluid'>
       <div className='row'>
         <div className='col'>
-          <img className='img-fluid 'style={{width:'100%',height:'400px'}} src="./Image/ERP-Development-Services-1.jpg" alt="" />
+          <img className='img-fluid ' style={{ width: '100%', height: '400px' }} src="./Image/ERP-Development-Services-1.jpg" alt="" />
         </div>
       </div>
       <div className='container'>
         <div className='row'>
+          <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, et laudantium quos, dolor natus sequi officiis aliquam recusandae harum autem voluptates fugiat dolores cumque! Praesentium blanditiis enim soluta ab id.</p>
+
           <div className='col-md-6'>
-            
+            <img className='img-fluid' src="./Image/ERP-Development-Services-3.jpg" alt="" />
+          </div>
+          <div className='col-md-6'>
+            <h1>heading</h1>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae hic illo reprehenderit quod rem! Tempore molestias inventore necessitatibus possimus? Quaerat doloribus totam, itaque asperiores quibusdam optio esse sequi tempore vel.</p>
+
+          </div>
+        </div>
+        <div className='row bg-dark'>
+          <div className='col-md-6'>
+            <div className='row'>
+              <div className='col-md-6'><div className="card" >
+                <img src="./Image/3D-rendering-5.png" className="card-img-top" style={{ width: '100%', height: '300px' }} alt="..." />
+                <div className="card-body">
+                  <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, sed alias saepe eaque doloremque aperiam, a in, cum eos beatae ad consequatur natus at? Laudantium saepe voluptate repellat itaque omnis.</p>
+                </div>
+              </div></div>
+              <div className='col-md-6'><div className="card" >
+                <img src="./Image/3D-rendering-5.png" className="card-img-top" style={{ width: '100%', height: '300px' }} alt="..." />
+                <div className="card-body">
+                  <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, sed alias saepe eaque doloremque aperiam, a in, cum eos beatae ad consequatur natus at? Laudantium saepe voluptate repellat itaque omnis.</p>
+                </div>
+              </div></div>
+            </div>
+            <div className='row'>
+              <div className='col-md-6'><div className="card" >
+                <img src="./Image/3D-rendering-5.png" className="card-img-top" style={{ width: '100%', height: '300px' }} alt="..." />
+                <div className="card-body">
+                  <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, sed alias saepe eaque doloremque aperiam, a in, cum eos beatae ad consequatur natus at? Laudantium saepe voluptate repellat itaque omnis.</p>
+                </div>
+              </div></div>
+              <div className='col-md-6'><div className="card" >
+                <img src="./Image/3D-rendering-5.png" className="card-img-top" style={{ width: '100%', height: '300px' }} alt="..." />
+                <div className="card-body">
+                  <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, sed alias saepe eaque doloremque aperiam, a in, cum eos beatae ad consequatur natus at? Laudantium saepe voluptate repellat itaque omnis.</p>
+                </div>
+              </div>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-6'>
+            <img className='img-fluid' src="./Image/ERP-Development-Services-4.jpg" alt="" />
           </div>
         </div>
       </div>
